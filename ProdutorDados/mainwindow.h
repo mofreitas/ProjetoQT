beb92619a0edba0172511c6adc883198b66d3396
mofreitas <matheusoliveirarn@hotmail.com>
2017-06-01 02:02:52 -0300

    Add files via upload

---
+++ ProdutorDados/mainwindow.h
@@ -0,0 +1,37 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTcpSocket>
+#include <QString>
+#include <QTimer>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+
+public slots:
+    void enviarDados();
+    void pararEnvio();
+    void comecarEnvio();
+    void conectarHost();
+    void desconectarHost();
+
+private:
+    Ui::MainWindow *ui;
+    QTcpSocket *socket;
+    int min, max;
+    QString ip_host;
+    QTimer *timer;
+};
+
+#endif // MAINWINDOW_H
