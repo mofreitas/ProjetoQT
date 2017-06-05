c8fbdde7e2f0d178a472b65503944bba59ba9ef4
mofreitas <matheusoliveirarn@hotmail.com>
2017-06-05 00:06:50 -0300

    Add files via upload

--- ConsumidorDados/mainwindow.cpp
+++ ConsumidorDados/mainwindow.cpp
@@ -1,7 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "widgetplotter.h"
 #include <QTcpSocket>
 #include <QDebug>
+#include <QDateTime>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -9,6 +11,7 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     socket = new QTcpSocket(this);
+    timer = new QTimer(this);
 
     connect(ui->conectarButton,
             SIGNAL(clicked(bool)),
@@ -21,7 +24,21 @@ MainWindow::MainWindow(QWidget *parent) :
     connect(ui->atualizarButton,
             SIGNAL(clicked(bool)),
             this,
-            SLOT(updatelista()));
+            SLOT(updateLista()));
+    connect(timer,
+            SIGNAL(timeout()),
+            this,
+            SLOT(leituraDados()));
+    connect(ui->comecarButton,
+            SIGNAL(clicked(bool)),
+            this,
+            SLOT(comecarPlotter()));
+    connect(ui->pararButton,
+            SIGNAL(clicked(bool)),
+            this,
+            SLOT(pararPlotter()));
+
+    //TEM QUE DAR "promote to" no botao direito do widget para relaciona-lo a uma classe
 }
 
 MainWindow::~MainWindow()
@@ -59,8 +76,14 @@ void MainWindow::updateLista()
         {
             if(socket->waitForReadyRead(3000))
             {
-
+                while(socket->bytesAvailable())
+                {
+                    ip_lista.append(socket->readLine());
+                }
             }
+            ui->listWidget->clear();
+            ui->listWidget->addItems(ip_lista);
+            qDebug() << "Lista atualizada";
         }
         else
         {
@@ -75,11 +98,147 @@ void MainWindow::updateLista()
 
 void MainWindow::comecarPlotter()
 {
+    ui->plotterWidget->limparVetor();
+    QList<QListWidgetItem*> itens_selecionados;
+    QString comandoGet;
+    if(socket->state()==QTcpSocket::ConnectedState)
+    {
+        //Perguntar ao professor (sempre necessário?)
+        if(socket->isOpen())
+        {
+            //Obtem lista de objetos selecionados
+            itens_selecionados=ui->listWidget->selectedItems();
+            //Monta o comando "get"
+            comandoGet="get " + itens_selecionados.at(0)->text();
+            //Envia o comando, lembrando que write recebe ponteiro para std::string
+            socket->write(comandoGet.toStdString().c_str());
+            //Espera 3s para enviar comando
+            if(socket->waitForBytesWritten(3000))
+            {
+                qDebug() << "Dados requeridos com sucesso";
+                //https://stackoverflow.com/questions/16023533/how-to-make-sure-that-readyread-signals-from-qtcpsocket-cant-be-missed
+                //waitForReadyRead() só pode ser chamado no mesmo slot do write
+                if(socket->waitForReadyRead(3000))
+                {
+                    timer->start(ui->timingSlider->value()*250);
+                }
+                else
+                {
+                    qDebug() << "Falha na leitura de dados";
+                }
+            }
+            else
+            {
+                qDebug() << "Falha na requisição de dados";
+            }
+        }
+    }
+    else
+    {
+        qDebug() << "Servidor desconectado, reconecte-o novamente";
+    }
 
+
+    /*QList<QListWidgetItem*> itens_selecionados;
+    QString comandoGet, dado;
+    QStringList linha, dados;
+    if(socket->state()==QTcpSocket::ConnectedState)
+    {
+        //Perguntar ao professor (sempre necessário?)
+        if(socket->isOpen())
+        {
+            //Obtem lista de objetos selecionados
+            itens_selecionados=ui->listWidget->selectedItems();
+            //Monta o comando "get"
+            comandoGet="get " + itens_selecionados.at(0)->text();
+            //Envia o comando, lembrando que write recebe ponteiro para std::string
+            socket->write(comandoGet.toStdString().c_str());
+            //Espera 3s para enviar comando
+            if(socket->waitForBytesWritten(3000))
+            {
+                qDebug() << "Dados requeridos com sucesso";
+            }
+            else
+            {
+                qDebug() << "Falha na requisição de dados";
+            }
+            //Espera 3s para leitura de dados
+            if(socket->waitForReadyRead(3000))
+            {
+                qDebug() << "Lendo Dados";
+                while(socket->bytesAvailable()) //Repete enquanto houver dados na lista de espera
+                {
+                    //ler a linha mandada pelo servidor, retirando os "\n" e "\n" que vem com os dados
+                    dado=socket->readLine().replace("\n", "").replace("\r", "");
+                    //LEr documentação QString::split -> Separa string em " " e retorna uma QStringList com as substrings;
+                    linha=dado.split(" ");
+                    //Verifica se tem apenas dois dados (hora e valor)
+                    if(linha.size()==2)
+                    {
+                        //Pega o segundo valor de dados
+                        dados.append(linha.at(1));
+                    }
+                }
+            }
+            else
+            {
+                qDebug() << "Falha na leitura de dados";
+            }
+            ui->plotterWidget->converterDados(dados);
+            timer->start(ui->timingSlider->value());
+        }
+    }
+    else
+    {
+        qDebug() << "Servidor desconectado, reconecte-o novamente";
+    }*/
 }
 
 void MainWindow::pararPlotter()
 {
+    timer->stop();
+    socket->readAll();
+    qDebug() << "Parando de obter dados do servidor";
+}
 
+void MainWindow::leituraDados()
+{
+    QString dado;
+    QStringList linha;
+    QDateTime dT;
+    if(socket->state()==QTcpSocket::ConnectedState)
+    {
+        //Perguntar ao professor (sempre necessário?)
+        if(socket->isOpen())
+        {
+            qDebug() << "Lendo Dados";
+            if(socket->bytesAvailable()) //Repete enquanto houver dados na lista de espera
+            {
+                //ler a linha mandada pelo servidor, retirando os "\n" e "\n" que vem com os dados
+                dado=socket->readLine().replace("\n", "").replace("\r", "");
+                //LEr documentação QString::split -> Separa string em " " e retorna uma QStringList com as substrings;
+                linha=dado.split(" ");
+                //Verifica se tem apenas dois dados (hora e valor)
+                if(linha.size()==2)
+                {
+
+                    dT = QDateTime::fromString(linha.at(0), Qt::ISODate);
+                    dado=linha.at(1);
+                    qDebug() << dT.toString(Qt::ISODate) << " " << QString::number(dado.toInt());
+
+                    ui->plotterWidget->setReta(dT, dado.toInt());
+                }
+            }
+            else
+            {
+                timer->stop();
+                qDebug() << "Leitura finalizada";
+            }
+        }
+    }
+    else
+    {
+        qDebug() << "Servidor desconectado, reconecte-o novamente";
+    }
 }
 
--- ConsumidorDados/widgetplotter.h
+++ ConsumidorDados/widgetplotter.h
@@ -1,19 +1,28 @@
 #ifndef WIDGETPLOTTER_H
 #define WIDGETPLOTTER_H
 
-#include <QMainWindow>
-#include <QObject>
 #include <QWidget>
+#include <vector>
+#include <QPaintEvent>
+#include <QDateTime>
+
+using namespace std;
 
 class widgetPlotter : public QWidget
 {
     Q_OBJECT
 public:
     explicit widgetPlotter(QWidget *parent = 0);
+    void paintEvent(QPaintEvent *e);
+    //void converterDados(QStringList &lista_dados);
+    void setReta(QDateTime dT, int y0);
+    void limparVetor();
 
 signals:
 
-public slots:
+private:
+    vector<int> dados;
+    QDateTime horaInicial;
 };
 
 #endif // WIDGETPLOTTER_H
