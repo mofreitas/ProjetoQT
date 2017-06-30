80d74adeb9097e3c2a1b73d0e56f88f0da1708ab
gabrielmcoelho <gabriel_mcoelho@hotmail.com>
2017-06-29 23:02:50 -0300

    Update widgetplotter.h

--- ConsumidorDados/widgetplotter.h
+++ ConsumidorDados/widgetplotter.h
@@ -14,13 +14,28 @@ class widgetPlotter : public QWidget
     Q_OBJECT
 public:
     explicit widgetPlotter(QWidget *parent = 0);
+    /**
+     * @brief paintEvent Função responsável pelo desenho do plotter e pela representação dos dados
+     * @param e Ponteiro para a classe QPaintEvent, responsável por acessar os métodos da mesma para realizar pinturas e desenhos no programa
+     */
     void paintEvent(QPaintEvent *e);
+    /**
+     * @brief desenharGrafico Função responsável por normalizar os dados de acordo com o intervalo de tempo em que os mesmos foram produzidos
+     * @param eX Vetor que contém os valores do eixo X recebidos pelo servidor
+     * @param eY Vetor que contém os valores do eixo Y recebidos pelo servidor
+     */
     void desenharGrafico(const vector<float> &eX, const vector<float> &eY);
 
 signals:
 
 private:
+    /**
+     * @brief eixoX Vetor para representar os valores do eixo X do plotter
+     */
     vector<float> eixoX;
+    /**
+     * @brief eixoY Vetor para representar os valores do eixo Y do plotter
+     */
     vector<float> eixoY;
 };
 
